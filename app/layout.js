5b3e8e0be624ab4fa5e5aa6de5325eb5df71dfd5
HariHara-sn <sudhanabirami007@gmail.com>
2025-07-29 19:01:29 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -7,17 +7,14 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
-import { icons } from "react-icons";
+// import { icons } from "react-icons";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "HariHaraSudhan - Software Developer",
   description:
     "This is the portfolio of Hari Hara Sudhan. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
-  icons: {
-    // icon: "/logo_top.png",
-    // icon:"profile.png",
-  },
+
 };
 
 export default function RootLayout({ children }) {
